eb7c8513ebbcd1ddecff2978defd60bf5090a045
mugunthraman <mugunth.raman@eaglegenomics.com>
2022-01-09 13:26:32 +0530

    removed comments

--- src/main/java/com/eagle/TestCases/HomeTest.java
+++ src/main/java/com/eagle/TestCases/HomeTest.java
@@ -25,84 +25,84 @@ public class HomeTest extends BaseTest{
 		objHome = new HomePage();	
 	}
 
-//	@Test(priority = 0, description = "Check the visibility of Logo")
-//	public void VerifyLogoVisibility_Test() throws InterruptedException, IOException{
-//		try{  	  
-//			objHome.verifyLogoVisibility();	
-//			onSuccessMeassage("Home logo is successfully verified");
-//		}
-//		catch(Exception | AssertionError ex){
-//			objHome.captureScreenshot("homeLogoFail");
-//			onFailreMeassage(ex.getMessage(),"homeLogoFail");					
-//		}
-//	}
-//
-//	@Test(priority = 1, description = "Check if dashBoard displays all the components")
-//	public void VerifyDashboard_Test() throws InterruptedException, IOException{
-//		try{
-//			objHome.verifyDashboardComponents();
-//			onSuccessMeassage("DashBoard displays all the components");
-//		}
-//		catch(Exception | AssertionError ex){
-//			objHome.captureScreenshot("dashboardFail");
-//			onFailreMeassage(ex.getMessage(),"dashboardFail");					
-//		}
-//	}
-//
-//	@Test(priority = 2, description = "Check for user information in profile")
-//	public void VerifyUserProfile() throws InterruptedException, IOException{
-//		try{
-//			String functionName = new Object(){}.getClass().getEnclosingMethod().getName();
-//
-//			System.out.println(functionName);
-//			String value = this.getConfiguration().get(functionName);
-//			String[] HomParameters =value.split(",");
-//			String ProfileName = HomParameters[0].trim();
-//
-//			objHome.verifyUserProfiles(ProfileName);
-//			onSuccessMeassage("User profile is successfully verified");
-//		}
-//		catch(Exception | AssertionError ex){
-//			objHome.captureScreenshot("profileFail");
-//			onFailreMeassage(ex.getMessage(),"profileFail");					
-//		}
-//	}
-//
-//	@Test(priority = 3, description = "Check for copyright and other links")
-//	public void VerifyCopyRights() throws InterruptedException, IOException{
-//		try{
-//			objHome.verifyCopyRights();
-//			onSuccessMeassage("CopyRight details are present");
-//		}
-//		catch(Exception | AssertionError ex){
-//			objHome.captureScreenshot("CopyRightsFail");
-//			onFailreMeassage(ex.getMessage(),"CopyRightsFail");					
-//		}
-//	}
-//
-//	@Test(priority = 4, description = "Search and Save the Global search")
-//	public void CreateGlobalSearch_Test() throws InterruptedException, IOException{
-//		try{
-//			objHome.createGlobalSearch();
-//			onSuccessMeassage("Search is successfully created and saved");
-//		}
-//		catch(Exception | AssertionError ex){
-//			objHome.captureScreenshot("SavesearchFail");
-//			onFailreMeassage(ex.getMessage(),"SavesearchFail");					
-//		}
-//	}
-//
-//	@Test(priority = 5, description = "Verify Saved Searches")
-//	public void VerifySavedSearches_Test() throws InterruptedException, IOException{
-//		try{
-//			objHome.verifySavedSearches();
-//			onSuccessMeassage("Search is successfully created and saved");
-//		}
-//		catch(Exception | AssertionError ex){
-//			objHome.captureScreenshot("SavesearchFail");
-//			onFailreMeassage(ex.getMessage(),"SavesearchFail");					
-//		}
-//	}
+	@Test(priority = 0, description = "Check the visibility of Logo")
+	public void VerifyLogoVisibility_Test() throws InterruptedException, IOException{
+		try{  	  
+			objHome.verifyLogoVisibility();	
+			onSuccessMeassage("Home logo is successfully verified");
+		}
+		catch(Exception | AssertionError ex){
+			objHome.captureScreenshot("homeLogoFail");
+			onFailreMeassage(ex.getMessage(),"homeLogoFail");					
+		}
+	}
+
+	@Test(priority = 1, description = "Check if dashBoard displays all the components")
+	public void VerifyDashboard_Test() throws InterruptedException, IOException{
+		try{
+			objHome.verifyDashboardComponents();
+			onSuccessMeassage("DashBoard displays all the components");
+		}
+		catch(Exception | AssertionError ex){
+			objHome.captureScreenshot("dashboardFail");
+			onFailreMeassage(ex.getMessage(),"dashboardFail");					
+		}
+	}
+
+	@Test(priority = 2, description = "Check for user information in profile")
+	public void VerifyUserProfile() throws InterruptedException, IOException{
+		try{
+			String functionName = new Object(){}.getClass().getEnclosingMethod().getName();
+
+			System.out.println(functionName);
+			String value = this.getConfiguration().get(functionName);
+			String[] HomParameters =value.split(",");
+			String ProfileName = HomParameters[0].trim();
+
+			objHome.verifyUserProfiles(ProfileName);
+			onSuccessMeassage("User profile is successfully verified");
+		}
+		catch(Exception | AssertionError ex){
+			objHome.captureScreenshot("profileFail");
+			onFailreMeassage(ex.getMessage(),"profileFail");					
+		}
+	}
+
+	@Test(priority = 3, description = "Check for copyright and other links")
+	public void VerifyCopyRights() throws InterruptedException, IOException{
+		try{
+			objHome.verifyCopyRights();
+			onSuccessMeassage("CopyRight details are present");
+		}
+		catch(Exception | AssertionError ex){
+			objHome.captureScreenshot("CopyRightsFail");
+			onFailreMeassage(ex.getMessage(),"CopyRightsFail");					
+		}
+	}
+
+	@Test(priority = 4, description = "Search and Save the Global search")
+	public void CreateGlobalSearch_Test() throws InterruptedException, IOException{
+		try{
+			objHome.createGlobalSearch();
+			onSuccessMeassage("Search is successfully created and saved");
+		}
+		catch(Exception | AssertionError ex){
+			objHome.captureScreenshot("SavesearchFail");
+			onFailreMeassage(ex.getMessage(),"SavesearchFail");					
+		}
+	}
+
+	@Test(priority = 5, description = "Verify Saved Searches")
+	public void VerifySavedSearches_Test() throws InterruptedException, IOException{
+		try{
+			objHome.verifySavedSearches();
+			onSuccessMeassage("Search is successfully created and saved");
+		}
+		catch(Exception | AssertionError ex){
+			objHome.captureScreenshot("SavesearchFail");
+			onFailreMeassage(ex.getMessage(),"SavesearchFail");					
+		}
+	}
 	
 	@Test(priority = 6, description = "Verify Contact help Desk")
 	public void VerifyContactHelpDesk_Test() throws InterruptedException, IOException{
